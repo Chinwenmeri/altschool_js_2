65e45e2adccd19b364a605582ff0edf2dd4a3b6d
Chukwudi <cchinwenmeri@gmail.com>
2025-12-23 12:25:25 +0100

    Adjustments made

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
-const enterTodo = document.getElementById('enter-todo')
-const todoEntry =document.getElementById('todo-Entry').value
+let students = []; 
 
-// console.log(todoEntry)
+const studentForm = document.getElementById('studentForm')
+const tableBody = document.getElementById('studentTableBody')
+const averageDisplay = document.getElementById('averageDisplay')
 
-const getTodo = function(){
-    console.log(todoEntry)
+studentForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const studentName = document.getElementById('studentName').value
+    const score = parseFloat(document.getElementById('studentScore').value)
+
+    students.push({studentName,score})
+
+    studentForm.reset()
+    render()
+
+    console.log(students)
+})
+
+function deleteStudent(index){
+    students.splice(index, 1)
+    render()
 }
 
-enterTodo.addEventListener('click', getTodo)
+function render() {
+    tableBody.innerHTML = ' '
+    let totalScore = 0
+
+    students.forEach((student, index) =>{
+        totalScore += student.score
+
+        const row = `
+        <tr>
+            <td>${student.studentName}</td>
+            <td>${student.score}</td>
+            <td><button onclick = "deleteStudent(${index})">Delete</button></td>
+        </tr>
+        `
+        tableBody.innerHTML += row
+    })
+
+    const avg = students.length === 0 ? 0 : (totalScore / students.length).toFixed(2);
+    averageDisplay.innerText = `Average Score: ${avg}`;
+}
